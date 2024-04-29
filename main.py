411defd274cccc791e77c61ba614182a4f5ee5e2
jaraycode <jonasaray12@gmail.com>
2024-04-29 10:34:44 -0400

    Cambiando la estructura
    El archivo hmtl ahora se encuentra en la carpeta templates para que la API con Flak la pueda reconocer y renderizar al momento de inicializar la aplicación.

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
+from flask import Flask, render_template
+import cv2
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
 if __name__ == "__main__":
-    print("Hola")
+    app.run()
