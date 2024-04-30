9e8fec3b46393fac1632c0d8e3dd2f8901b9d066
jaraycode <jonasaray12@gmail.com>
2024-04-29 22:02:51 -0400

    Pruebas
    Las pruebas ya genera una respuesta para poder identificar una letra

--- main.py
+++ main.py
@@ -2,6 +2,16 @@ from flask import Flask, render_template
 import cv2
 app = Flask(__name__)
 
+def preview(image):
+    
+    # Asegurar escala de grises
+    imagen = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    # Transformar a forma binaria
+    imagen = cv2.threshold(imagen,400,600,cv2.THRESH_BINARY)
+
+    pass
+
+
 @app.route('/')
 def home():
     return render_template('index.html')
